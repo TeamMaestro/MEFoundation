6028ee6bc07d21f43e0aa6a20ce03ca634c1d132
William Towe <willbur1984@gmail.com>
2014-03-14 16:50:36 -0400

    Initial Commit

---
+++ MEFoundation/MEFoundation.h
@@ -0,0 +1,13 @@
+//
+//  MEFoundation.h
+//  MEFoundation
+//
+//  Created by William Towe on 3/14/14.
+//  Copyright (c) 2014 Maestro, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MEFoundation : NSObject
+
+@end
---
+++ MEFoundation/MEFoundation.m
@@ -0,0 +1,13 @@
+//
+//  MEFoundation.m
+//  MEFoundation
+//
+//  Created by William Towe on 3/14/14.
+//  Copyright (c) 2014 Maestro, LLC. All rights reserved.
+//
+
+#import "MEFoundation.h"
+
+@implementation MEFoundation
+
+@end
