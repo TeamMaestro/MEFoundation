2bc88a5c8a6ccd6dc78018085b6c0216837d63aa
Wilhelmina Drengwitz <willa@eksdyne.com>
2016-05-12 14:55:05 -0400

    Create a demo project that uses the MEFoundation as a Pod

---
+++ MEFoundationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MEFoundationDemo
+//
+//  Created by Willa Drengwitz on 5/12/16.
+//  Copyright © 2016 Maestro, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, "8.0"
+
+use_frameworks!
+
+pod "MEFoundation", :path => './'
+
+link_with "MEFoundationDemo"
+workspace "Demo"
